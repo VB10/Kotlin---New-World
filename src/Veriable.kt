52f5f7612a8e485fb49f07c0aabd24007bc971c8
VB <vb@VB10.local>
2017-05-27 00:08:34 +0300

    Ders2
    Kotlin Giriş Ders 2

---
+++ src/Veriable.kt
@@ -0,0 +1,55 @@
+
+//main ve çift  tab yaptığımızda otomatik gleiyor
+
+
+fun main(args: Array<String>) {
+
+    // Yorum satırı
+    /*
+    //Yorum Paragrafı
+           1
+     var name= "Kotlin"
+          var born=2017
+           2
+       var name:String= "Kotlin2-"
+       var born:Int =2017
+       3
+        var name:String?=null
+    name="VB"
+    print(name)
+    4
+     val PI: Double =3.45
+
+    PI=2.4;
+    6
+
+    print("Adınızı giriniz:")
+    var name = readLine()
+
+    print("Yaşınız:")
+    var age:Int = readLine()!!.toInt()
+
+    print("Paranız:")
+    var money:Double = readLine()!!.toDouble()
+
+    println("Adınız:"+name)
+    println("Yaşınız:"+age)
+    println("Paranız:"+money)
+       */
+
+    var x1:Int?
+    var x2:String = "10"
+
+    x1=x2.toInt()
+
+    var yPI:Double=3.14
+    var yPIint:Int = yPI.toInt()
+
+    println("x1:"+x1)
+    println("x2:"+x2)
+    println("yPI:"+yPI)
+    println("yPInt:"+yPIint)
+
+
+
+}
