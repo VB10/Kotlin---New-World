d6f418e000bb6b21cb6aa25f7f5e74b3d2b03cc7
VB <vb@VB10.local>
2017-05-30 18:33:20 +0300

    Lesson 3
    Decision Making and Simple Loop

---
+++ src/DecisionMaking.kt
@@ -0,0 +1,56 @@
+
+/*
+
+1
+   var n1:Int = 5;
+    var n2:Int= 6;
+2
+    print(5>6)
+    print(6>5)
+    print(5==5)
+3
+    if(n1>n2){
+        println("$n1 büyüktür $n2 den")
+    }else {
+        print("$n2 büyüktür $n1 den")
+    }
+ 4
+        println(15>10 &&  6<4) // ve kontrolünde ikisininde doğru olması aranır
+    println(15>10 ||  6<4) // veya kontrolünde ikinsinden biri doğru olması yeter
+
+
+5
+    var name :String?
+    print("bir kelime giriniz:")
+    name= readLine()
+
+    if (name=="veli"){
+        println("1.denemede başardınız")
+    }
+    else if(name=="ali"){
+        println("2.denemede başardınız")
+
+    }
+    else if(name=="fatma"){
+        println("3.denemede başardınız")
+
+    }
+    else if(name =="ahmet") {
+        println("4.denemede başardınız")
+    }
+    else {
+        println("Başaramadınız")
+    }
+
+
+
+*/
+
+fun main(args: Array<String>) {
+
+    for (number in 1..5){
+        print("$number ")
+
+    }
+
+}
